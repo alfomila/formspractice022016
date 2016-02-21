76c31a53a5ec2459ba8b94031eb478ec58c2a7ab
Christine Fruehling <alfomila@gmail.com>
2016-02-20 20:35:08 -0800

    Show new edits. New - Title and h1. New labels. New changes in html and javascript.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,22 @@
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput = $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val();
-    var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val();
+    var yourNameInput = $("input#yourName").val();
+    var pastTenseVerbInput = $("input#pastTenseVerb").val();
+    var pluralNounInput = $("input#pluralNoun").val();
+    var exclamation1Input = $("input#exclamation1").val();
+    var exclamation2Input = $("input#exclamation1").val();
+    var noun1Input = $("input#noun1").val();
+    var modeOfTransportationInput = $("input#modeOfTransportation").val();
+    var noun2Input = $("input#noun2").val();
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
+    $(".yourName").text(yourNameInput);
+    $(".pastTenseVerb").text(pastTenseVerbInput);
+    $(".pluralNoun").text(pluralNounInput);
+    $(".exclamation1").text(exclamation1Input);
+    $(".exclamation2").text(exclamation2Input);
+    $(".noun1").text(noun1Input);
+    $(".modeOfTransportation").text(modeOfTransportationInput);
+    $(".noun2").text(noun2Input);
 
     $("#story").show();
 
