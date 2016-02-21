7bf7059f480c69049ee090b682e90967022a0e2a
Christine Fruehling <alfomila@gmail.com>
2016-02-20 21:15:32 -0800

    More new labels reflectingchanges in html and javascript.

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,27 @@
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
     var yourNameInput = $("input#yourName").val();
-    var pastTenseVerbInput = $("input#pastTenseVerb").val();
     var pluralNounInput = $("input#pluralNoun").val();
     var exclamation1Input = $("input#exclamation1").val();
     var exclamation2Input = $("input#exclamation1").val();
     var noun1Input = $("input#noun1").val();
+    var placeInput = $("input#place").val();
     var modeOfTransportationInput = $("input#modeOfTransportation").val();
+    var typeOfFoodInput = $("input#typeOfFoodInput").val();
     var noun2Input = $("input#noun2").val();
+    var amountInput = $("input#amount").val();
+    var noun3Input = $("input#noun3").val();
 
     $(".yourName").text(yourNameInput);
-    $(".pastTenseVerb").text(pastTenseVerbInput);
     $(".pluralNoun").text(pluralNounInput);
     $(".exclamation1").text(exclamation1Input);
     $(".exclamation2").text(exclamation2Input);
     $(".noun1").text(noun1Input);
+    $(".place").text(placeInput);
     $(".modeOfTransportation").text(modeOfTransportationInput);
     $(".noun2").text(noun2Input);
+    $(".amount").text(amountInput);
+    $(".noun3").text(noun3Input);
 
     $("#story").show();
 
